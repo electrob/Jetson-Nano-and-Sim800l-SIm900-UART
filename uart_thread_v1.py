61d4d02286bde9deb700239640a1bd821e6bba7f
electrob <aman.kashyap47@gmail.com>
2020-08-18 04:03:29 +0530

    added new working code
    Added: 
    New at commands
    Random values generator
    Random data sending interval

--- uart_thread_v1.py
+++ uart_thread_v1.py
@@ -3,6 +3,12 @@ import threading
 import time
 import serial
 from enum import IntEnum
+import random
+import queue
+
+q = queue.Queue(maxsize = 3)
+serial_lock = threading.Lock()
+queue_lock = threading.Lock()
 
 class ATResp(IntEnum):
     ErrorNoResponse=-1
@@ -22,6 +28,7 @@ def sendATCmdWaitReturnResp(cmd, response, timeout=.5, interByteTimeout=.1):
         """
         This function is designed to return data and check for a final response, e.g. 'OK'
         """
+        serial_lock.acquire() 
         print("Send AT Command: {}".format(cmd))
         serial_port.timeout=timeout
         serial_port.inter_byte_timeout=interByteTimeout
@@ -36,14 +43,21 @@ def sendATCmdWaitReturnResp(cmd, response, timeout=.5, interByteTimeout=.1):
         lines=[l for l in lines if len(l) and not l.isspace()]
         print("Lines: {}".format(lines))
 
-        if not len(lines): return (ATResp.ErrorNoResponse, None)
-
+        if not len(lines):
+            serial_lock.release() 
+            return (ATResp.ErrorNoResponse, None)
+    
         _response=lines.pop(-1)
         print("Response: {}".format(_response))
-        if not len(_response) or _response.isspace(): return (ATResp.ErrorNoResponse, None)
-        elif response==_response: return (ATResp.OK, lines)
+        if not len(_response) or _response.isspace(): 
+            serial_lock.release() 
+            return (ATResp.ErrorNoResponse, None)
+        elif response==_response: 
+            serial_lock.release() 
+            return (ATResp.OK, lines)
+        serial_lock.release() 
         return (ATResp.ErrorDifferentResponse, None)
-
+        
 
 def gsm_start():
     sendATCmdWaitReturnResp("ATE0", "OK")
@@ -80,27 +94,60 @@ def gsm_gprs_setup(apn, user, pwd):
     #sendATCmdWaitReturnResp("AT+CIPSHUT", "OK")
     #sendATCmdWaitReturnResp("AT+CIPSHUT", "OK")
 
-def gsm_send_data(data):
+def gsm_send_data():
     sendATCmdWaitReturnResp("AT+SAPBR=2,1", "OK")
     sendATCmdWaitReturnResp("AT+SAPBR=1,1", "OK")
     sendATCmdWaitReturnResp("AT+HTTPINIT", "OK")
-    sendATCmdWaitReturnResp("AT+HTTPPARA=\"URL\",\"http://iotapi.aronasoft.com/helpers/functions.php?type=arduinoApiTest&count=1&temp=12&humidity=31.2\"", "OK")
-    sendATCmdWaitReturnResp("AT+HTTPPARA=\"CID\",1","OK");
-    sendATCmdWaitReturnResp("AT+HTTPACTION=0","OK");
-    sendATCmdWaitReturnResp("\r\n","", timeout=7);
+
+    while True:
+        #print("IN TRUE")
+       # queue_lock.acquire()
+        if q.full():
+            print("IN TRUE FULL")
+            queue_lock.acquire()
+            count = str(q.get())
+            temp = str(q.get())
+            humidity = str(q.get())
+            sendATCmdWaitReturnResp("AT+HTTPPARA=\"URL\",\"http://iotapi.aronasoft.com/helpers/functions.php?type=arduinoApiTest&count="+count+"&temp="+temp+"&humidity="+humidity+"\"", "OK")
+            sendATCmdWaitReturnResp("AT+HTTPPARA=\"CID\",1","OK");
+            sendATCmdWaitReturnResp("AT+HTTPACTION=0","OK");
+            sendATCmdWaitReturnResp("\r\n","", timeout=7);
+            queue_lock.release() 
+
+def gsm_data_generator():
+    while True:
+        print("IN GEN")
+        randTime = random.randint(1,9)
+        print(randTime)
+        time.sleep(randTime)
+        queue_lock.acquire()
+        if not q.full():
+            val = random.randint(35,84)
+            q.put(val)
+        queue_lock.release();
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+    
+   # lock = threading.Lock()
 
     gsm_start = threading.Thread(target=gsm_start)
     gsm_start.start()
-    time.sleep(3)
+    #time.sleep(3)
     gsm_gprs_setup = threading.Thread(target=gsm_gprs_setup, args=("airtelgprs.com","",""))
     gsm_gprs_setup.start()
     
-    time.sleep(14)
-    starttcp = threading.Thread(target=gsm_send_data, args=("ce2da385-1b8f-43c1-8622-c4376c50142c.mock.pstmn.io",))
-    starttcp.start()
+    time.sleep(26)
+    
+    data_generator = threading.Thread(target=gsm_data_generator, args=())
+    data_generator.start()
+    
+   # time.sleep(2)
+    gsm_send_data = threading.Thread(target=gsm_send_data, args=())
+    gsm_send_data.start()
+
+#    data_generator = threading.Thread(target=gsm_data_generator, args=())
+ #   data_generator.start()
